95a03e50f1be29d82251f5abb7aa2e333cce79bf
wqmeepo <627822626@qq.com>
2020-08-12 16:27:28 +0800

    1、支持配置文件配置任务，执行路径、启停时间、启停状态判断，配置文件与脚本同层级，格式为： { 	"task1": { 		"taskid": "1", 		"taskname": "转换机-资讯", 		"taskpath": "D:/转换机/newtran -资讯/hs_Trans.exe", 		"starthour": "16", 		"startmin":"10", 		"endhour": "17", 		"endmin":"30", 		"starttatus": "on", 		"endstatus":"on" 	}, 	"task2": { 		"taskid": "2", 		"taskname": "转换机-行情", 		"taskpath": "D:/转换机/newtran - 行情/hs_Trans.exe", 		"starthour": "16", 		"startmin":"10", 		"endhour": "17", 		"endmin":"30", 		"starttatus": "on", 		"endstatus":"on" 	} } 2、启动和停止任务分开执行

--- timedTasks.py
+++ timedTasks.py
@@ -1,4 +1,78 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import datetime
+import os, json, logging
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+class AutoMaintenance(object):
+    def __init__(self, config_path):
+        self.job_stores = {
+            'default': MemoryJobStore(),
+        }
+        self.executors = {
+            'default': ThreadPoolExecutor(20),
+            'processpool': ProcessPoolExecutor(10)
+        }
+        self.job_defaults = {
+            'coalesce': False,
+            'max_instances': 5
+        }
+        self.config_path = config_path
+
+    def jsonParse(self):
+        with open(self.config_path + '\\taskconfig.json', 'r', encoding='utf-8')as fp:
+            json_data = json.load(fp)
+        return json_data
+
+    def jobStart(self, excute_path):
+        print(excute_path + '-----start')
+        print(type(excute_path))
+
+    def jobEnd(self, excute_path):
+        print(excute_path + '-----end')
+
+    def jobStartGenerator(self):
+        json_data = self.jsonParse()
+        scheduler = BlockingScheduler(jobstores=self.job_stores, executors=self.executors,
+                                      job_defaults=self.job_defaults)
+        for data in json_data:
+            if json_data[data]['starttatus'] == 'on' or 'ON':
+                scheduler.add_job(self.jobStart, trigger='cron', args=(json_data[data]['taskpath'],),
+                                  id=json_data[data]['taskname'] + ' start',
+                                  day_of_week='mon-fri',
+                                  hour=json_data[data]['starthour'],
+                                  minute=json_data[data]['startmin'],
+                                  jitter=1)
+        try:
+            print('job_start--begin')
+            scheduler.start()
+        except SystemExit:
+            print('job_start--faild')
+            exit()
+
+    def jobEndGenerator(self):
+        json_data = self.jsonParse()
+        scheduler = BlockingScheduler(jobstores=self.job_stores, executors=self.executors,
+                                      job_defaults=self.job_defaults)
+        for data in json_data:
+            if json_data[data]['endstatus'] == 'on' or 'ON':
+                scheduler.add_job(self.jobEnd, trigger='cron', args=(json_data[data]['taskpath'],),
+                                  id=json_data[data]['taskname'] + ' end',
+                                  day_of_week='mon-fri',
+                                  hour=json_data[data]['endhour'],
+                                  minute=json_data[data]['endmin'],
+                                  jitter=1)
+        try:
+            print('job_end--begin')
+            scheduler.start()
+        except SystemExit:
+            print('job_end--faild')
+            exit()
+
+
+if __name__ == '__main__':
+    config_path = os.getcwd()
+    print(config_path)
+    auto_maintenance = AutoMaintenance(config_path)
+    auto_maintenance.jobStartGenerator()
+    auto_maintenance.jobEndGenerator()
