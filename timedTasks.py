97289b127526e61048e45a7199ede4a1631729f9
wqmeepo <627822626@qq.com>
2020-08-13 15:48:00 +0800

    增加开启、关闭处理方法

--- timedTasks.py
+++ timedTasks.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import os, json, logging
+import os, json, logging, subprocess, time
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
@@ -18,55 +18,46 @@ class AutoMaintenance(object):
             'max_instances': 5
         }
         self.config_path = config_path
+        self.process_list = []
 
     def jsonParse(self):
         with open(self.config_path + '\\taskconfig.json', 'r', encoding='utf-8')as fp:
             json_data = json.load(fp)
         return json_data
 
-    def jobStart(self, excute_path):
-        print(excute_path + '-----start')
-        print(type(excute_path))
+    def jobStart(self, *args):
+        print(args[1] + ' 任务启动')
+        self.process_list.append(subprocess.Popen(args[0]))
 
-    def jobEnd(self, excute_path):
-        print(excute_path + '-----end')
+    def jobEnd(self):
+        print('任务进程关闭开始')
+        for process in self.process_list:
+            process.kill()
+            time.sleep(2)
+        self.process_list = []
 
     def jobStartGenerator(self):
         json_data = self.jsonParse()
         scheduler = BlockingScheduler(jobstores=self.job_stores, executors=self.executors,
                                       job_defaults=self.job_defaults)
         for data in json_data:
-            if json_data[data]['starttatus'] == 'on' or 'ON':
-                scheduler.add_job(self.jobStart, trigger='cron', args=(json_data[data]['taskpath'],),
+            if json_data[data]['startstatus'] == 'on' or 'ON':
+                scheduler.add_job(self.jobStart, trigger='cron',
+                                  args=(json_data[data]['taskpath'], json_data[data]['taskname']),
                                   id=json_data[data]['taskname'] + ' start',
                                   day_of_week='mon-fri',
                                   hour=json_data[data]['starthour'],
                                   minute=json_data[data]['startmin'],
-                                  jitter=1)
-        try:
-            print('job_start--begin')
-            scheduler.start()
-        except SystemExit:
-            print('job_start--faild')
-            exit()
-
-    def jobEndGenerator(self):
-        json_data = self.jsonParse()
-        scheduler = BlockingScheduler(jobstores=self.job_stores, executors=self.executors,
-                                      job_defaults=self.job_defaults)
-        for data in json_data:
-            if json_data[data]['endstatus'] == 'on' or 'ON':
-                scheduler.add_job(self.jobEnd, trigger='cron', args=(json_data[data]['taskpath'],),
-                                  id=json_data[data]['taskname'] + ' end',
+                                  jitter=3)
+                scheduler.add_job(self.jobEnd, trigger='cron',
                                   day_of_week='mon-fri',
                                   hour=json_data[data]['endhour'],
-                                  minute=json_data[data]['endmin'],
-                                  jitter=1)
+                                  minute=json_data[data]['endmin'], )
         try:
-            print('job_end--begin')
+            print('任务监控开始')
             scheduler.start()
         except SystemExit:
-            print('job_end--faild')
+            print('任务监控结束')
             exit()
 
 
@@ -75,4 +66,3 @@ if __name__ == '__main__':
     print(config_path)
     auto_maintenance = AutoMaintenance(config_path)
     auto_maintenance.jobStartGenerator()
-    auto_maintenance.jobEndGenerator()
