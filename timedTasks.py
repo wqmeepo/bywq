0ac999f1cbdb2cf97e4c35981c7e75371f9f2256
wqmeepo <627822626@qq.com>
2020-08-13 18:36:26 +0800

    bug修复：修复pyinstaller安装后运行报错：No trigger by the name cron" was found"

--- timedTasks.py
+++ timedTasks.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import os, json, logging, subprocess, time, random
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.triggers.cron import CronTrigger
 
 
 class AutoMaintenance(object):
@@ -51,17 +52,20 @@ class AutoMaintenance(object):
         for data in json_data:
             if json_data[data]['startstatus'] == 'on':  # 判断任务是否启动
                 # 启动调度器设置，写死为按每周1-周五执行，小时与分钟支持配置文件配置
-                scheduler.add_job(self.jobStart, trigger='cron',
+                trigger_start = CronTrigger(
+                    day_of_week='mon-fri',
+                    hour=json_data[data]['starthour'],
+                    minute=json_data[data]['startmin'], )
+                scheduler.add_job(self.jobStart, trigger_start,
                                   args=(json_data[data]['taskpath'], json_data[data]['taskname']),
                                   id=json_data[data]['taskname'] + ' start',
-                                  day_of_week='mon-fri',
-                                  hour=json_data[data]['starthour'],
-                                  minute=json_data[data]['startmin'], )
+                                  )
                 # 关闭调度器设置
-                scheduler.add_job(self.jobEnd, trigger='cron',
-                                  day_of_week='mon-fri',
-                                  hour=json_data[data]['endhour'],
-                                  minute=json_data[data]['endmin'], )
+                trigger_stop = CronTrigger(
+                    day_of_week='mon-fri',
+                    hour=json_data[data]['endhour'],
+                    minute=json_data[data]['endmin'], )
+                scheduler.add_job(self.jobEnd, trigger_stop)
         try:  # 获取调度器启动异常
             print('任务监控开始，目前监视中的任务有：\n')
             for data in json_data:
